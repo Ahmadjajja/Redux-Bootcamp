13353c3d4f25c29862c96aeb770915374b59bd0e
Ahmad jajja <ahmadjajja86@gmail.com>
2023-06-24 15:09:17 +0500

    project1: counter app

---
+++ redux-only/counter-app/counter.js
@@ -0,0 +1,85 @@
+// steps:
+const { createStore } = require("redux");
+// Initial state
+const initialState = {
+  count: 0,
+};
+// actions - action -action creator
+// increments
+// decrements
+// reset
+// increaseByAmount
+
+// action
+
+//increase Action creator
+const incrementAction = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+//decrease Action creator
+const decrementAction = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+//reset Action creator
+const resetAction = () => {
+  return {
+    type: "RESET",
+  };
+};
+//increase by amount Action creator
+const increaseByAmountAction = (anyAmount) => {
+  return {
+    type: "INCREASE_BY_AMOUNT",
+    payload: anyAmount,
+  };
+};
+
+// reducers
+const counterReducer = (state = initialState, action) => {
+  // Reducer always listen to the action to perform business logic
+  console.log(action);
+  if (action.type == "INCREMENT") {
+    return {
+      count: state.count + 1,
+    };
+  }
+  if (action.type == "DECREMENT") {
+    return {
+      count: state.count - 1,
+    };
+  }
+  if (action.type == "RESET") {
+    return {
+      count: 0,
+    };
+  }
+  if (action.type == "INCREASE_BY_AMOUNT") {
+    return {
+      count: state.count + action.payload,
+    };
+  }
+};
+// store
+
+const store = createStore(counterReducer);
+
+// subscribe to store
+store.subscribe(() => {
+  // get state
+  const stateData = store.getState();
+  console.log(stateData);
+});
+
+// dispatch action
+
+// store.dispatch(incrementAction());
+// store.dispatch(resetAction());
+// store.dispatch(decrementAction());
+
+// dispatch action with payload
+store.dispatch(increaseByAmountAction(100)); // stateDate = 100
+store.dispatch(increaseByAmountAction(100)); // stateDate = 200
